785fa91ac9ed14b69c713ef6d3a23666abb36b20
Ricardo Oliveira <Ricardo Oliveira>
2018-04-26 12:04:16 -0300

    Add Formularios e Widgets: dicas, treinamentos

---
+++ Fluig-Project-Widgets/forms/Treinamentos/displayFields.js
@@ -0,0 +1,1 @@
+function displayFields(form,customHTML){var partnerTitle=form.getValue("partner");partnerTitle=partnerTitle.replaceAll('"',"'");form.setValue("partner",partnerTitle);var partnerBenefits=form.getValue("partnerBenefits");partnerBenefits=partnerBenefits.replaceAll('"',"'");form.setValue("partnerBenefits",partnerBenefits)};
